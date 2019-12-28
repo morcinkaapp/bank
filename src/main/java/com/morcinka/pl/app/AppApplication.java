bd4e9d6e52fd1e57c70f3a7aee7c6317b22eb347
klem <klemst2@wp.pl>
2019-12-28 18:25:23 +0100

    created project

---
+++ src/main/java/com/morcinka/pl/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.morcinka.pl.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
