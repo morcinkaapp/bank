2b70d115ac437e87a48591a1010e36de4e8c2fe4
klem <klemst2@wp.pl>
2019-12-30 12:40:41 +0100

    added configuration

--- src/main/java/user/repository/UserRepository.java
+++ src/main/java/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import user.UserEntity;
 
 /**
  * @author Jarosław Kaczmarek
  */
+@Repository
 public interface UserRepository extends JpaRepository<Long, UserEntity> {
 }
